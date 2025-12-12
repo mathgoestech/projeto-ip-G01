eb281bd6dd0e8e53991b9b2608d33b612c9f9997
Maria Clara Melo <clarad3oliveira@gmail.com>
2025-12-12 18:08:59 -0300

    iniciei a modularização
    - criação do módulo settings.py para concentrar a visualização e (se necessário) alteração das constantes
    - limpeza e simplificação do main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import pygame
 import sys
+from settings import *
 
 pygame.init()
 
-# configurações da janela (dimesões em pixel, criação do display e título)
-largura, altura = 1000, 600
-tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Resgate RelâmpagOZ')
-
-# propriedades base do objeto/player (elphaba)
-elph_x = 100
-elph_y = 200
-velocidade_elph = 7 # determina o quão rápida a elphaba se move (velocidade horizontal em pixels/frame)
+# configurações da janela (criação do display e título)
+tela = pygame.display.set_mode((tela_largura, tela_altura))
+pygame.display.set_caption(título)
 
 # carrega a imagem do arquivo e redimensiona ela para ter o mesmo tamanho da janela
-tela_fundo = pygame.transform.scale(pygame.image.load('imagens/backgrounds/red_castle.jpg'), (largura, altura))
+tela_fundo = pygame.transform.scale(
+    pygame.image.load('imagens/backgrounds/red_castle.jpg'),
+    (tela_largura, tela_altura)
+)
 
 def draw(player, x, y): # função de renderização
 
-    # redesenha a imagem de fundo e a elphaba na sua posição atual, atualizando o novo quadro
+    # redesenha a imagem de fundo e a Elphaba na sua posição atual, atualizando o novo quadro
     tela.blit(tela_fundo, (0, 0))
     tela.blit(player, (x, y))
     pygame.display.update()
@@ -26,16 +24,14 @@ def draw(player, x, y): # função de renderização
 
 # inicializa o player e redimensiona a imagem pra um tamanho adequado
 player = pygame.image.load('imagens/sprites/elphaba/elphie-idle.png')
-player = pygame.transform.scale(player, (400, 400))
+player = pygame.transform.scale(player, (elph_largura, elph_altura))
 
 # controle de tempo (fps)
 clock = pygame.time.Clock() # limita a taxa de quadros por segundo
 
 # variáveis de controle do pulo
 elph_pula = False
-gravidade = 1 # aceleração vertical em pixels/frame
-altura_pulo = 20 # altura máxima do pulo em pixels
-velocidade_pulo = altura_pulo # velocidade vertical atual
+velocidade_pulo = pulo_altura # velocidade vertical atual
 
 # game loop (roda enquanto o jogo tá aberto)
 while True:
@@ -49,11 +45,11 @@ while True:
 
     # movimentação horizontal
     if keys[pygame.K_LEFT]:
-        elph_x -= velocidade_elph # decrementa x, movendo para a esquerda
+        elph_x -= elph_velocidade # decrementa x, movendo para a esquerda
     if keys[pygame.K_RIGHT]:
-        elph_x += velocidade_elph # incrementa x, movendo para a direita
+        elph_x += elph_velocidade # incrementa x, movendo para a direita
 
-    # mecanica do pulo
+    # mecânica do pulo
     if keys[pygame.K_UP] and not elph_pula:
         elph_pula = True # ativa o estado de pulo
 
@@ -61,11 +57,11 @@ while True:
     if elph_pula:
         elph_y -= velocidade_pulo # altera a coordenada y, movendo objeto/player para cima
         velocidade_pulo -= gravidade # aplica a gravidade, desacelerando a velocidade do pulo na subida
-        if velocidade_pulo < -altura_pulo: # termina quando a velocidade do pulo atinge o valor inicial de subida
-            piso = altura - 400
+        if velocidade_pulo < -pulo_altura: # termina quando a velocidade do pulo atinge o valor inicial de subida
+            piso = tela_altura - elph_altura
             elph_y = piso
             elph_pula = False
-            velocidade_pulo = altura_pulo # reseta a velocidade para o próximo pulo
+            velocidade_pulo = pulo_altura # reseta a velocidade para o próximo pulo
 
     draw(player, elph_x, elph_y) # chama a função que desenha o cenário e os objetos na tela
     clock.tick(60) # controle de frame rate (60 fps)
---
+++ settings.py
@@ -0,0 +1,16 @@
+# definições gerais (dimensões da tela em pixels, título e fps)
+tela_largura = 1000
+tela_altura = 600
+título = 'Resgate RelâmpagOZ'
+fps = 60
+
+# propriedades base da Elphie
+elph_velocidade = 7 # determina o quão rápida a Elphie se move (velocidade horizontal em pixels/frame)
+elph_largura = 400
+elph_altura = 400
+elph_x = 100
+elph_y = 200
+
+# física do movimento vertical
+gravidade = 1 # aceleração vertical em pixels/frame
+pulo_altura = 20 # altura máxima do pulo em pixels
